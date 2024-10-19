9b049e3289633b585b53f2ff4cb5694f85e0bbca
AmNotAGoose <alexyihengwu@gmail.com>
2024-10-19 16:38:39 -0400

    homepage updates

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './Home.css'
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './Home.css';
 
 function Home() {
-  const [count, setCount] = useState(0)
+  const swappingText = ["iPhone X", "iPhone 12", "Galaxy S10"];
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTextIndex(
+        () => Math.floor(Math.random() * (swappingText.length))
+      );
+    }, 300);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className="hero">
+        <div className="hero-content">
+          <h1>Your <span>{swappingText[currentTextIndex]}</span>, </h1>
+          <h1>ready for a second life.</h1>
+          <p>Find out how your device can be restored easily, at home!</p>
+          <button className="hero-button">
+            Take me there!
+          </button>
+        </div>
+      </section>
+
+      <section className="main-section dark">
+        <div className="container">
+          <h2>placeholder</h2>
+        </div>
+      </section>
+
+      <section className="main-section light">
+        <div className="container">
+          <h2>placeholder</h2>
+          <div className='card-container'>
+            <div className="card">
+              <h2>ajwirfeijfraewij</h2>
+              <p>asdfdasfgrweagtasfrgdgarefs</p>
+            </div>
+            <div className="card">
+              <h2>ajwirfeijfraewij</h2>
+              <p>asdfdasfgrweagtasfrgdgarefs</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="main-section dark">
+        <div className="container">
+          <h2>placeholder</h2>
+          <div className="logos">
+            <img src={reactLogo} alt="React Logo" className="logo" />
+            <img src={viteLogo} alt="Vite Logo" className="logo" />
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
